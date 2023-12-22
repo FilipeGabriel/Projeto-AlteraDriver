14d36abc8058b4c36fb53c20690d1cfb97df0e37
Filipe Gabriel <filipegabriel11@hotmail.com>
2023-12-21 22:01:59 -0300

    Implemented front-end with javafx

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -1,29 +1,46 @@
-package application;
+package gui;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
+import java.util.ResourceBundle;
 
-public class Program {
+import gui.util.Alerts;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 
-	public static void main(String[] args) {
+public class ViewController implements Initializable {
+
+	private List<String> list = new ArrayList<>();
+	
+	@FXML
+	private TextField textFieldPath;
+
+	@FXML
+	private Button modfyButton;
+
+	@FXML
+	public void onTextFieldPathAction() {
+		textFieldPath.getText();
+	}
+
+	@FXML
+	public void onModfyButtonAction() throws FileNotFoundException, IOException {
 		
-		Scanner sc = new Scanner(System.in);
-		List<String> list = new ArrayList<>();
-		
-		System.out.print("Insira o caminho bilhetes desejado: ");
-		String filePath = sc.nextLine();
-		
-		try (BufferedReader br = new BufferedReader(new FileReader(filePath))){
+		try (BufferedReader br = new BufferedReader(new FileReader(textFieldPath.getText()))){
 			
 			Integer number = 0;			
 			String rl = br.readLine();
@@ -55,7 +72,7 @@ public class Program {
 			
 			Integer nameDriverNumber = 0;
 			
-			File instantiatePath = new File(filePath);
+			File instantiatePath = new File(textFieldPath.getText());
 			
 			String saveModifiedFile = instantiatePath.getParent() + "\\newgltPabx.txt";
 			
@@ -63,7 +80,7 @@ public class Program {
 			
 			boolean exist = Files.exists(getFilePath);
 					
-				for(int i=0; exist == true; i++) {
+				for(int i=2; exist == true; i++) {
 					nameDriverNumber = i;
 					saveModifiedFile = instantiatePath.getParent() + "\\newgltPabx" + nameDriverNumber + ".txt";
 				
@@ -78,13 +95,23 @@ public class Program {
 				}	
 			}
 			catch (IOException e){
-				System.out.println("Erro: " + e);
+				Alerts.showAlert("IO Exception", "Error to modify the Driver", e.getMessage(), AlertType.ERROR);
 			}
+			
+			Alerts.showConfirmation("Driver modification", AlertType.CONFIRMATION);
+			
+			list.clear();
+			
 		}
 		catch (IOException e){
-			System.out.println("Erro: " + e);
+			Alerts.showAlert("IO Exception", "Error to reade the file", e.getMessage(), AlertType.ERROR);
 		}
-		sc.close();
+
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle resourceBundle) {
+
 	}
 
 }
